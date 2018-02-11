ceea327c8ccfb124412e313a962fc76a30663ac5
antonpavlov <anton.it.pavlov@gmail.com>
2018-02-11 21:10:38 +0300

    Initial commit

---
+++ src/main/java/ru/appavlov/library/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.appavlov.library;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibraryApplication.class);
+	}
+
+}
